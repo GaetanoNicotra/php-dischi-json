2bb03b8ffc0ce21f7b0d1a7f11d01ff552399ff6
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-12-05 11:49:53 +0100

    [UPD] creating a orm with input fields to insert a new disk

--- index.php
+++ index.php
@@ -13,23 +13,79 @@ require_once("./server.php");
 </head>
 
 <body>
-  <div class="container mt-4">
+      <div class="container">
+          <h1>PHP music</h1>
+
+       <!-- form -->
+<form action="">
+  <div class="container">
     <div class="row">
-      <?php foreach($dischi_tradotti as $disco){ ?>
-        <div class="col-4 mb-3">
-          <div class="card h-100">
-            <img src="..." class="card-img-top" alt="Copertina">
-            <div class="card-body">
-              <h5 class="card-title"><?= $disco['artista'] ?></h5>
-              <p class="card-text"><?= $disco['titolo'] ?></p>
-              <p class="card-text"><?= $disco['anno'] ?></p>
-              <p class="card-text"><?= $disco['genere'] ?></p>
-            </div>
-          </div>
+
+      <!-- COLONNA SINISTRA -->
+      <div class="col-4">
+        <div class="mb-3">
+          <label class="form-label">Inserisci il titolo</label>
+          <input type="text" class="form-control">
         </div>
-      <?php } ?>
+
+        <div class="mb-3">
+          <label class="form-label">Inserisci il nome</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Inserisci l'anno</label>
+          <input type="text" class="form-control">
+        </div>
+      </div>
+
+      <!-- COLONNA DESTRA -->
+      <div class="col-4">
+        <div class="mb-3">
+          <label class="form-label">Inserisci il genere</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Inserisci il link</label>
+          <input type="text" class="form-control">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label">Inserisci l'immagine</label>
+          <input type="text" class="form-control">
+        </div>
+      </div>
+
     </div>
+
+    <button class="btn btn-primary mt-3">Invia</button>
   </div>
+</form>
+
+
+<!-- header -->
+
+
+        <!-- Card -->
+        <div class="container mt-4">
+          <div class="row">
+          <?php foreach($dischi_tradotti as $disco){ ?>
+            <div class="col-3 mb-3">
+                <div class="card h-100">
+                  <img src="<?= $disco['cover'] ?>" class="card-img-top" alt="Copertina"  style="max-height:250px;" >
+                  <div class="card-body">
+                  <h5 class="card-title"><?= $disco['artista'] ?></h5>
+                  <p class="card-text"><?= $disco['titolo'] ?></p>
+                  <p class="card-text"><?= $disco['anno'] ?></p>
+                  <p class="card-text"><?= $disco['genere'] ?></p>
+                  <a class="card-text"><?= $disco['url'] ?></a>
+                </div>
+              </div>
+            </div>
+          <?php } ?>
+        </div>
+      </div>
 </body>
 
 </html>
